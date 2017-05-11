a5f95bed193da6053d9cbae9887098faa4617309
Joe Garcia <joe.garcia@cyberark.com>
2017-05-11 12:49:52 -0400

    Updated AIMPHPExample.php to allow insecure CURL connections

--- AIMPHPExample.php
+++ AIMPHPExample.php
@@ -1,28 +1,33 @@
 <?php
 
-$request = new HttpRequest();
-$request->setUrl('https://components.joe-garcia.local/AIMWebService/api/Accounts');
-$request->setMethod(HTTP_METH_GET);
+$curl = curl_init();
 
-$request->setQueryData(array(
-  'AppID' => 'RESTExamples',
-  'Safe' => 'T-APP-CYBR-RESTAPI',
-  'Folder' => 'Root',
-  'Object' => 'Database-MicrosoftSQLServer-JG-sql01.joe-garcia.local-Svc_BambooHR'
+curl_setopt_array($curl, array(
+  CURLOPT_URL => "https://192.168.2.151/AIMWebService/api/Accounts?AppID=RESTExamples&Safe=T-APP-CYBR-RESTAPI&Folder=Root&Object=Database-MicrosoftSQLServer-JG-sql01.joe-garcia.local-Svc_BambooHR",
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => "",
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 30,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => "GET",
+  CURLOPT_SSL_VERIFYPEER => false,
+  CURLOPT_SSL_VERIFYHOST => false,
+  CURLOPT_HTTPHEADER => array(
+    "cache-control: no-cache",
+    "content-type: application/json",
+    "postman-token: 3c378df1-c2e9-a66f-fd10-141fbf6a3bd8"
+  ),
 ));
 
-$request->setHeaders(array(
-  'postman-token' => '31c53b65-fa46-521b-faef-d5b356211bc4',
-  'cache-control' => 'no-cache',
-  'content-type' => 'application/json'
-));
+$response = curl_exec($curl);
+$err = curl_error($curl);
 
-try {
-  $response = $request->send();
+curl_close($curl);
 
-  echo $response->getBody();
-} catch (HttpException $ex) {
-  echo $ex;
+if ($err) {
+  echo "cURL Error #:" . $err;
+} else {
+  echo $response;
 }
 
 ?>
